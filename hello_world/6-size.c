2d80e99998d225be35ab7851b207cf4996b26928
Brahian-A <10114@holbertonstudents.com>
2024-10-17 21:05:34 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - 6 de c 
+ * main - 6 de c
  * Return: 0
  */
 int main(void)
 {
-         printf("Size of char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of int: %zu byte(s)\n", sizeof(intType));	
-    printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-    printf("Size of double: %zu byte(s)\n", sizeof(doubleType));
-    printf("Size of long: %zu byte(s)\n", sizeof(longType));
-    printf("Size of long long: %zu byte(s)\n", sizeof(Type));
-    printf("Size of short: %zu byte(s)\n", sizeof(Type));
-	return (0);
+printf("Tamaño de char: %zu byte(s)\n", sizeof(char));
+printf("Tamaño de int: %zu byte(s)\n", sizeof(int));
+printf("Tamaño de float: %zu byte(s)\n", sizeof(float));
+printf("Tamaño de double: %zu byte(s)\n", sizeof(double));
+printf("Tamaño de long: %zu byte(s)\n", sizeof(long));
+printf("Tamaño de long long: %zu byte(s)\n", sizeof(long long));
+printf("Tamaño de short: %zu byte(s)\n", sizeof(short));
+return (0);
 }
