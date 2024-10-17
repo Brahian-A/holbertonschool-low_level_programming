6c2f69546d5cc4c0c55e66c3005763899216d627
Brahian-A <10114@holbertonstudents.com>
2024-10-17 20:54:16 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - 6 de c 
+ * Return: 0
+ */
+int main(void)
+{
+         printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));	
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+    printf("Size of double: %zu byte(s)\n", sizeof(double));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+    printf("Size of short: %zu byte(s)\n", sizeof(short));
+	return (0);
+}
