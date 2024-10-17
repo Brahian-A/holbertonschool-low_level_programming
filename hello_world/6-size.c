81820d2c073def0f61aac41dc97e37f88fa69ac1
Brahian-A <10114@holbertonstudents.com>
2024-10-17 21:11:31 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,9 +8,7 @@ int main(void)
 printf("Tamaño de char: %zu byte(s)\n", sizeof(char));
 printf("Tamaño de int: %zu byte(s)\n", sizeof(int));
 printf("Tamaño de float: %zu byte(s)\n", sizeof(float));
-printf("Tamaño de double: %zu byte(s)\n", sizeof(double));
-printf("Tamaño de long: %zu byte(s)\n", sizeof(long));
+printf("Tamaño de loni int: %zu byte(s)\n", sizeof(long int));
 printf("Tamaño de long long: %zu byte(s)\n", sizeof(long long));
-printf("Tamaño de short: %zu byte(s)\n", sizeof(short));
 return (0);
 }
