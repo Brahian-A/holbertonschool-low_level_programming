a71840b204dd6f1fdb064d308a435ffb50edd867
Brahian-A <10114@holbertonstudents.com>
2024-10-21 18:18:07 +0000

    commit_message

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *main - 2-print_alphabet.c
+ *Return: 0
+ */
+
+int main(void)
+{
+	char az;
+
+
+
+	for (az = 'a'; az <= 'z'; az++)
+
+{	putchar(az);
+
+}
+	for (az = 'A'; az <= 'Z'; az++)
+{	putchar(az);
+
+}
+
+putchar('\n');
+
+return (0);
+}
