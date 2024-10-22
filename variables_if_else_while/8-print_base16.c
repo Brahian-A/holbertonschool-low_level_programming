0911b7887b71a2b896719b3e892a1b8a91df67b6
Brahian-A <10114@holbertonstudents.com>
2024-10-22 19:06:50 +0000

    8/1

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *main- 6-print_numberz.c
+ *Return: 0
+ */
+        int main(void)
+{
+char exa[] ="123456789abcdf";
+int a;
+
+        for (a = 0; a <16; a++)
+{       putchar(exa[a]);
+
+}
+
+        putchar('\n');
+return (0);
+
+}
