86206049bb080b91d61586bb37b316defffa4c71
Brahian-A <10114@holbertonstudents.com>
2024-10-22 18:52:08 +0000

    7/1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *main - 7-print_tebahpla.c
+ *Return: 0
+ */
+
+int main(void)
+{
+	char za;
+
+
+
+	for (za = 'z'; za >= 'a'; za--)
+
+	putchar(za);
+
+
+putchar('\n');
+
+return (0);
+}
