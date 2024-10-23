afa60fa4a0ca1c0911b1462c4dd204c3587c2596
Brahian <10114@holbertonstudents.com>
2024-10-23 14:48:27 +0000

    1/1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet- Que dificil me la pusiste 
+ *Return: void 
+ */
+
+
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+{	_putchar(az);
+}
+
+
+
+	_putchar('\n'); 
+
+
+}
