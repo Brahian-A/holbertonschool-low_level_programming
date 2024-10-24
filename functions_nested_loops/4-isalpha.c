f1f1ece9813dc3c6988a5a0bbcd45ec13b8ea903
root <10114@holbertonstudents.com>
2024-10-24 17:49:09 +0000

    commit_message

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isalpha- Nunca entiendo bien el ejercicio
+ * @c: letras del alfabeto
+ *Return: 1 if letter 0 isnt
+ */
+
+
+int _isalpha(int c)
+{
+
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+
+
+}
