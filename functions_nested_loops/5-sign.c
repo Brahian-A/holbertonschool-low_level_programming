0f1d51edb17e4b92b8b0092bfd5bfc4134cbf8ac
root <10114@holbertonstudents.com>
2024-10-24 21:19:31 +0000

    commit_message

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - Muestra el signo de un númer
+ * @n: variable de signos
+ *
+ * Return: 1 si es positivo, 0 si es cero, -1 si es negativo
+ */
+
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+
+	_putchar('+');
+	return (1);
+
+	}
+	else if (n == 0)
+	{
+
+	_putchar('0');
+	return (0);
+	}
+
+	else
+	{
+
+	_putchar('-');
+	return (-1);
+	}
+}
