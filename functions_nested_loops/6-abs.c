e259775f445d060d686faf503860408d11fb2314
root <10114@holbertonstudents.com>
2024-10-24 22:54:11 +0000

    espero y funcione

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,15 +3,15 @@
 /**
  *_abs- función que calcula el valor absoluto de un número ente
  *Return: retorna el valor asboluto si es negativo lo miltiplica por -1
- *
+ *@r: variable que cree porque no sabia como hacer
  */
 
 int _abs(int r)
 {
-	if (r < 0 )
-		return -r;
+	if (r < 0)
+		return (-r);
 	else
-		return r;
+		return (r);
 
 
 }
