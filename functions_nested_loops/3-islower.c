75853aea0191331fe5f202a6f7c70c18d7014ac8
root <10114@holbertonstudents.com>
2024-10-24 16:36:05 +0000

    commit_message

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /**
- *int _islower(int c);
+ *_islower- Nunca entiendo bien el ejercicio 
  *Return: void
  */
 
 
 int _islower(int c)
+{
 
+	if (c <= 'a' && c >= 'z')
+		return (1);
+	else 
+		return (0);
+
+
+}
