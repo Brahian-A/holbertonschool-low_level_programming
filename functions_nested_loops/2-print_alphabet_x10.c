768dcaea0e3667497af80c22e517cd84c36fcf8c
root <10114@holbertonstudents.com>
2024-10-24 14:28:06 +0000

    commit_message

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * --------- es preba arreglalo cuando lo veas va encamindado 
- *Return:
+ * print_alphabet_x10- Lo voy entendiendo mejor lo mismo que el anterior por 10
+ *Return: void
  */
 
 
 
 
+
+void print_alphabet_x10(void)
+{
+	char az;
+	int d;
+
+	for (d = 0; d <= 10; d++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+			_putchar(az);
+		_putchar('\n');
+	}
+}
