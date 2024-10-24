76a24fca01035bb95d60a8501b63e7fc0e779543
root <10114@holbertonstudents.com>
2024-10-24 17:06:09 +0000

    commit_message

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  *_islower- Nunca entiendo bien el ejercicio 
- *Return: void
+ *Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
 
 
 int _islower(int c)
 {
 
-	if (c <= 'a' && c >= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else 
 		return (0);
