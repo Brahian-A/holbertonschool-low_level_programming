33c25b75c8b273cba1d59f844b2ec5cdfacc7519
Brahian-A <10114@holbertonstudents.com>
2024-10-25 19:08:15 +0000

    commit_message

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *times_table- la maldita tabla en la cual llevo 3 horas 
+ *
+ *Return: una tabla de multiplicar 
+ */
+
+
+void times_table(void)
+{
+int num, reps, asci;
+
+
+        for (reps = 0; reps < 10; reps ++)
+        {
+                for (num = 0; num < 10; num++)
+		{
+			asci = num * reps;
+
+			if(num * reps >= 10 )
+			{
+				_putchar('0' + (asci / 10));
+				_putchar('0' + (asci % 10));
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{	
+				_putchar('0' + (num * reps));
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+				_putchar('\n');
+        }
+}
