af69bce901a4636918b14afb50e7f5d5a71e4e84
Brahian-A <10114@holbertonstudents.com>
2024-10-25 14:22:17 +0000

    commit_message

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_last_digit- imprime el ultimo numero 
+ *@r: el numero a verificar 
+ *Return: el ultimo digito
+ */
+
+
+
+int print_last_digit(int r)
+{
+	int n;
+
+	n = r % 10;
+
+	if (n < 0)
+		n = -n;
+
+
+	_putchar (n + '0');
+	return (n);
+
+}
