6499935cbdfefcf2aa7e31688eb0fb648281bcd0
Brahian-A <10114@holbertonstudents.com>
2024-10-28 18:27:11 +0000

    commit_message

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,10 @@ int f, c;
 		{
 			if (c >= 10)
 			{
-			_putchar (c / 10 + '0' && c % 10 + '0');
+			_putchar (c / 10 + '0');
 			}
-			else
-			_putchar (c + '0');
+			_putchar (c % 10 + '0');
+
 		}
 
 
