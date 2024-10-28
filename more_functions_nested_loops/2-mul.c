03faca69360013df38951a09bb640bb95e8f46d2
Brahian-A <10114@holbertonstudents.com>
2024-10-28 13:16:42 +0000

    commit_message

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul- funcion que multiplica 2 variables
+ *@a: variable a a multiplicar
+ *@b: variable b a multiplicar
+ *Return: a multiplicado por b
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
