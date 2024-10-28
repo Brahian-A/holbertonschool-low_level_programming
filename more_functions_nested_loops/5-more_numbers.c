fd9e3e8988a1ba1256b6fdc4c3d6e730ea2491a3
Brahian-A <10114@holbertonstudents.com>
2024-10-28 15:01:19 +0000

    commit_message

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *more_numbers- del 0 al 14
+ *Return: del 0 al 14
+ *
+ */
+
+void more_numbers(void)
+{
+int f, c;
+
+	for (f = 0; f <= 10; f++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+			{
+			_putchar (c / 10 + '0');
+			_putchar (c % 10 + '0');
+			}
+			else
+			_putchar (c + '0');
+		}
+
+
+			_putchar ('\n');
+
+	}
+}
