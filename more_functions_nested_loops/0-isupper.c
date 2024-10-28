e93c9692f4f932a2f8a6c36d4b3954899b38384b
Brahian-A <10114@holbertonstudents.com>
2024-10-28 12:51:54 +0000

    commit_message

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper- funcion que busca mayusculas
+ *@c: variable que da mayusculas
+ *Return: 1 si es mayuscula 0 si no
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A'&& c <= 'Z')
+
+	return(1);
+	else
+	return(0);
+}
