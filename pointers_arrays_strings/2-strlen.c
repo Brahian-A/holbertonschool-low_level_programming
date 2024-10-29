162ac729174c80d6be0a8876a909027fcb6b4308
Brahian-A <10114@holbertonstudents.com>
2024-10-29 14:47:23 +0000

    commit_message

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen-
+ *@*s:
+ *Return:
+ *
+ */
+
+
+int _strlen(char *s)
+{
+size_t length = strlen(s)
+_putchar (s /10 + '0');
+_putchar (s %10 + '0');
+
+
+}
