25832352a1f8ccecc9c7b1acb014088fea60e9ce
Brahian-A <10114@holbertonstudents.com>
2024-10-30 14:53:21 +0000

    commit_message

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ *_puts- la funcion
+ *@str: el puntero
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+
+int a;
+
+	while (str[a] != '\0')
+	{
+		putchar(str[a]);
+		a++;
+
+	}
+
+}
