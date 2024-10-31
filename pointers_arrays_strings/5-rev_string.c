525d52c9d9fe4cfcfaf54ea5546b9656dcc66706
Brahian-A <10114@holbertonstudents.com>
2024-10-31 19:11:06 +0000

    commit_message

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,20 +10,18 @@ void rev_string(char *s)
 int a = 0;
 int b = 0;
 char c;
-        while (s[a] != '\0')
-        {
-                a++;
-        }
-        a--;
+	while (s[a] != '\0')
+	{
+	a++;
+	}
+	a--;
 
-        while (b < a)
+	while (b < a)
 {
- 	c = s[b];
-        s[b] = s[a];
-        s[a] = c;
-        b++;
-        a--;
-
-
+	c = s[b];
+	s[b] = s[a];
+	s[a] = c;
+	b++;
+	a--;
 }
 }
