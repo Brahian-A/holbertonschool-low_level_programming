02644a4f8a94700a9e8a908b41d4364a9f3c4882
Brahian-A <10114@holbertonstudents.com>
2024-10-31 19:08:53 +0000

    commit_message

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *rev_string- la funcnion
+ *@s: el puntero
+ *
+ */
+void rev_string(char *s)
+{
+int a = 0;
+int b = 0;
+char c;
+        while (s[a] != '\0')
+        {
+                a++;
+        }
+        a--;
+
+        while (b < a)
+{
+ 	c = s[b];
+        s[b] = s[a];
+        s[a] = c;
+        b++;
+        a--;
+
+
+}
+}
