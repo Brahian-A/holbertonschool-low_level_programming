da55d82ad0ff9bdd0c763a0b02c456a21f10f927
Brahian-A <10114@holbertonstudents.com>
2024-11-02 22:37:57 +0000

    commit_message

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcpy- funcio para copiar el string
+ *@src:el otro puntero
+ *@dest:puntero que guarda el destino
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *original = dest;
+int i = 0;
+	while (src[i] != '\0')
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+		*dest = '\0';
+		return (original);
+}
