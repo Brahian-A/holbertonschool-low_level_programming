045d5597972725f6d75cf7f74a300872669be2bd
Brahian-A <10114@holbertonstudents.com>
2024-11-02 09:23:07 +0000

    commit_message

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *print_array- el array
+ *@a: una variable 
+ *@n: la otra variable
+ *Return: pepe
+ *
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+	for (i = 0; i < n; i++)
+	{	printf("%d", a[i]);
+	}
+		if (i < n -1)
+		{	printf(", ");
+		}
+	putchar('\n');
+}
