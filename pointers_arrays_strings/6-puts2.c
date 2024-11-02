88f3414663f62fdc391170b1e5a344529fe185ff
Brahian-A <10114@holbertonstudents.com>
2024-11-02 01:18:23 +0000

    commit_message

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *puts2- la funcion
+ *@str: el string
+ *Return: una cadena
  *
  */
 
 void puts2(char *str)
 {
-int i;
+int i = 0;
 
 	while (str[i] != '\0')
-{
+	{
+		if (i % 2 == 0)
+		{
+
 		_putchar (str[i]);
-		i += 2;
-}
-_putchar ('\n');
+		}
+	i++;
+	}
+	_putchar ('\n');
 
 }
