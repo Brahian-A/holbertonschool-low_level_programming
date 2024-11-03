71d22bfce6286fe934df2a56f76e2218cd74340d
Brahian-A <10114@holbertonstudents.com>
2024-11-03 21:23:04 +0000

    commit_message

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *reverse_array- la funcion
+ *@a: inicio del bucle
+ *@n:final
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+int *inicio = a;
+int *final = a + n - 1;
+
+	while (inicio < final)
+	{
+		n = *inicio;
+		*a = *final;
+		*final = n;
+		inicio++;
+		final--;
+	}
+
+}
