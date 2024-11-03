5dcfba761e08b1515376cb136d37143f32108075
Brahian-A <10114@holbertonstudents.com>
2024-11-03 23:52:23 +0000

    commit_message

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *string_toupper- funcion que cambia minusculas a mayusculas
+ *@string; el string a cambiar 
+ *Return: 'a' - 'A' = a,b,c = A,B,C
+ *
+ *
+ *
+ *
+ */
+
+char *string_toupper(char *string)
+{
+char *original = string;
+
+	while (*string)
+	{	if (*string >= 'a' && *string <= 'z')
+		{	*string = *string - ('a' - 'A');
+
+		}
+
+		string++;
+	}
+	return (original);
+}
