ed59abae4f4d7162c21f6738b9c6c37c1f05d414
Brahian-A <10114@holbertonstudents.com>
2024-11-03 18:14:59 +0000

    commit_message

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,24 @@
 
 int _atoi(char *s)
 {
-int signo,
+	int i = 0;
+	unsigned int resultado = 0;
+	int signo = 1;
 
-	for (*s <= '0'; *s > '9'; s++) 
-		
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 0 && s[i] <= 9)
+		{
+			resultado = resultado * 10 + s[i] - 0;
+			if (s[i + 1] < 0 || s[i + 1] > 9)
+				break;
+		}
 
-
-
-
-
-
+		if (s[i] == '-')
+		{
+			signo = signo * -1;
+		}
+	}
+	resultado = resultado * signo;
+	return (resultado);
 }
