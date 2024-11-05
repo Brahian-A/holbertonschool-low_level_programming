678c27f086a0ba4dda9558ec4ba8fab8808818c7
Brahian-A <10114@holbertonstudents.com>
2024-11-05 13:28:27 +0000

    commit_message

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strchr- la funcion que buca caracteres
+ *@s: string de caracteres donde buscar c
+ *@c: variable con los caracters a encontrar
+ *Return: los caracteres que tiene c encontrados en el string *s
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+				s++;
+
+	}
+	return (NULL);
+
+}
