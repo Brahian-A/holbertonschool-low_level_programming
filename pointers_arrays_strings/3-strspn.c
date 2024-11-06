1ff097b25ced8e4e813794732b4ba4b021377cb6
Brahian-A <10114@holbertonstudents.com>
2024-11-06 21:17:22 +0000

    commit_message

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,20 +1,30 @@
 #include "main.h"
-
 /**
- *
- *
- *
- *
+ *_strspn- funcion que compara los contenidos de los punteros
+ *@s:puntero con el contenido
+ *@accept:puntero con el verificador
+ *Return: numero de caracteres verificados
  *
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+char *a = accept;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
 
 
 
-
-
-
-
+	while (*a)
+	{
+		if (*s == *a)
+		{
+			return (1 + _strspn(s + 1, accept));
+		}
+		a++;
+	}
+return (0);
 }
