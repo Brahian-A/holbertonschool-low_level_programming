b238c007f66856b1f3732fb334dd2b03d9eecf7c
Brahian-A <10114@holbertonstudents.com>
2024-11-10 23:33:24 +0000

    commit_message

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ *main- imprime el nombre del programa
+ *@argv:numero de argumentos
+ *@argc:numero de argumentos
+ *Return:pepe
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 0)
+	printf("%s\n", argv[0]);
+		return (0);
+}
