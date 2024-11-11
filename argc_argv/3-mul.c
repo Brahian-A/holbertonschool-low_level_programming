ade0b891c4b26588b727a3268ecc1eb011f31da1
Brahian-A <10114@holbertonstudents.com>
2024-11-11 00:48:02 +0000

    commit_message

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
 	{
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[2]);
+	
 		printf("%d\n", num1 * num2);
 	}
 		else
 		{
 			printf("Error\n");
 		}
-return(0);
+return (0);
 }
