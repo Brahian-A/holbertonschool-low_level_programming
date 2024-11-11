51cf8b2c3b65fe5431295f4c6b2e41eb2d360470
Brahian-A <10114@holbertonstudents.com>
2024-11-11 19:39:04 +0000

    commit_message

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,6 +3,23 @@
 #include <string.h>
 
 /**
+ *_longitud- funcion que busca el tama√o de la string
+ *@str: string a verificar el tama√o
+ *Return: el largo de la string
+ */
+
+size_t _longitud(const char *str)
+{
+	size_t longi = 0;
+
+	while (str[longi] != '\0')
+	{
+		longi++;
+	}
+		return (longi);
+}
+
+/**
  *_strdup- funcion que duplica la string
  *@str: string a duplicar
  *Return: la string duplicada
@@ -10,7 +27,7 @@
 
 char *_strdup(char *str)
 {
-char *dupstr = malloc(strlen(str) + 1);
+char *dupstr = malloc((long) +1);
 char *inicio = dupstr;
 
 	if (str == NULL)
