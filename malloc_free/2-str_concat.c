9d674281c0af357e9a13a2e222720e561342dd20
Brahian-A <10114@holbertonstudents.com>
2024-11-11 21:42:45 +0000

    commit_message

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,6 +3,24 @@
 #include <string.h>
 
 /**
+ *_longitud- funcion que busca el tamao de la string
+ *@s: string a verificar el tamao
+ *Return: el largo de la string
+ */
+
+size_t _longitud(const char *s)
+{
+	size_t longi = 0;
+
+	while (s[longi] != '\0')
+	{
+		longi++;
+	}
+	return (longi);
+}
+
+
+/**
  *str_concat- funcion que concatena 2 strings
  *@s1:string 1
  *@s2:string 2
@@ -12,14 +30,31 @@
 
 char *str_concat(char *s1, char *s2)
 {
-char *s3 = malloc(strlen(s1) + strlen(s2) + 1);
+	char *s3;
+	size_t i = 0;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
 
 	if (s3 == NULL)
 	{
 		return (NULL);
 	}
 
-		strcpy(s3, s1);
-		strcat(s3, s2);
-		return (s3);
+		while (*s1)
+		{
+			s3[i++] = *s1++;
+		}
+
+		while (*s2)
+		{
+			s3[i++] = *s2++;
+		}
+	s3[i] = '\0';
+
+	return (s3);
 }
