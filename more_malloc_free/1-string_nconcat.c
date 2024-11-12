994738258161a1ca8285ac1c224153b91789026d
Brahian-A <10114@holbertonstudents.com>
2024-11-12 17:12:34 +0000

    commit_message

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_longitud- funcion que busca el tamao de la string
+ *@s: string a verificar el tamao
+ *Return: el largo de la string
+ */
+
+size_t _longitud(const char *s)
+{
+        size_t longi = 0;
+
+        while (s[longi] != '\0')
+        {
+                longi++;
+        }
+        return (longi);
+}
+
+
+/**
+ *str_concat- funcion que concatena 2 strings
+ *@s1:string 1
+ *@s2:string 2
+ *Return: s3 con s1 y despues s2
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)// copiaste el 2 del anterior proyecto asi que vas a tener que cambiar cosas como que se concatena n veces
+{
+        char *s3;
+        size_t i = 0;
+
+        if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
+
+        s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
+
+        if (s3 == NULL)
+        {
+                return (NULL);
+        }
+
+                while (*s1)
+                {
+                        s3[i++] = *s1++;
+                }
+
+                while (*s2)
+                {
+                        s3[i++] = *s2++;
+                }
+        s3[i] = '\0';
+
+        return (s3);
+}
