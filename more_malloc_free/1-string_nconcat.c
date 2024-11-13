010fb36804d9c49d79e85210961b3fb063a0dc53
Brahian-A <10114@holbertonstudents.com>
2024-11-13 13:51:44 +0000

    commit_message

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,41 +24,59 @@ size_t _longitud(const char *s)
  *str_concat- funcion que concatena 2 strings
  *@s1:string 1
  *@s2:string 2
- *Return: s3 con s1 y despues s2
+ *Return: s3 con s1 y s2 n cantidad de veces
  */
 
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)// copiaste el 2 del anterior proyecto asi que vas a tener que cambiar cosas como que se concatena n veces
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *s3;
-        size_t i = 0;
+	char *s3;
+	size_t i = 0, j = 0;
 
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
-
-        s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
-
-        if (s3 == NULL)
-        {
-                return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
         }
 
-                while (*s1)
-                {
-                        s3[i++] = *s1++;
-                }
+		if (n < (_longitud(s2)))
+		{
+		s3 = malloc(_longitud(s1) + n + 1);
+		}
+		else
+		{
+			s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
+		}
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
 
-                while (*s2)
-                {
-                        s3[i++] = *s2++;
-                }
-        s3[i] = '\0';
+		while (*s1)
+		{
+			s3[i++] = *s1++;
+		}
 
-        return (s3);
+		if (n < (_longitud(s2)))
+		{
+			for (j = 0; j <= n; j++)
+			{
+				s3[i++] = *s2++;
+			}
+				
+		}
+
+		else
+		{
+			while (*s2)
+			{
+			s3[i++] = *s2++;
+			}
+		}
+		s3[i] = '\0';
+
+	return (s3);
 }
