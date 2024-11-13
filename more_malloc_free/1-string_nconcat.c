df759fe02c3c43a8951089e3e4f82d1e9304d9e1
Brahian-A <10114@holbertonstudents.com>
2024-11-13 14:01:19 +0000

    commit_message

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  *_longitud- funcion que busca el tamao de la string
  *@s: string a verificar el tamao
  *Return: el largo de la string
  */
-
 size_t _longitud(const char *s)
 {
-        size_t longi = 0;
+	size_t longi = 0;
 
-        while (s[longi] != '\0')
-        {
-                longi++;
-        }
-        return (longi);
+	while (s[longi] != '\0')
+	{
+		longi++;
+	}	return (longi);
 }
-
-
 /**
- *str_concat- funcion que concatena 2 strings
+ *string_nconcat- funcion que concatena 2 strings
  *@s1:string 1
  *@s2:string 2
+ *@n:las veces que se tiene que repetir n
  *Return: s3 con s1 y s2 n cantidad de veces
  */
-
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
@@ -40,35 +34,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 	{
 		s2 = "";
-        }
-
-		if (n < (_longitud(s2)))
+	}
+	if (n < (_longitud(s2)))
 		{
 		s3 = malloc(_longitud(s1) + n + 1);
 		}
 		else
-		{
-			s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
+		{s3 = malloc(_longitud(s1) + _longitud(s2) + 1);
 		}
 	if (s3 == NULL)
-	{
-		return (NULL);
-	}
-
-		while (*s1)
+	{	return (NULL);
+	}	while (*s1)
 		{
 			s3[i++] = *s1++;
 		}
-
 		if (n < (_longitud(s2)))
 		{
 			for (j = 0; j <= n; j++)
 			{
 				s3[i++] = *s2++;
 			}
-				
 		}
-
 		else
 		{
 			while (*s2)
@@ -77,6 +63,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			}
 		}
 		s3[i] = '\0';
-
 	return (s3);
 }
