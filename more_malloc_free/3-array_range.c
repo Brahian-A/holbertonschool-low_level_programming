67952f640e3c7636cac941676bdb3753f63c6c71
Brahian-A <10114@holbertonstudents.com>
2024-11-14 14:21:19 +0000

    commit_message

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,13 +2,34 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
- *
- *
+ *array_range- funcion que asigan espacion en la memoria a un array
+ *@min: rango minimo
+ *@max:rango maximo
+ *Return: pepe
  */
 
 
 int *array_range(int min, int max)
+{
+	int size, i;
+	int *array;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+			size = max - min + 1;
+		array =(int *)malloc(size * sizeof(int));
+			if (array == NULL)
+			{
+				return (NULL);
+			}
+
+		for (i = 0; i < size; i++)
+		{
+			array[i] = min + i;
+		}
+
+	return (array);
+
+}
