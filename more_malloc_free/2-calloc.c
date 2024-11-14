721ae3e9a9f7c2be50c7130e95f820b4dd9d9bc5
Brahian-A <10114@holbertonstudents.com>
2024-11-14 13:10:32 +0000

    commit_message

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,23 +2,34 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
- *
+ *_calloc- funcion que asigna memoria
+ *@nmemb: número de elementos que quero almacenar
+ *@size: tamaño de cada elemento
+ *Return:pepe
  */
 
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	if (nmemb <= 0)
+	int size_total, i;
+	void *ptr;
+
+	if (nmemb <= 0 || size <= 0)
 	{
 		return (NULL);
 	}
 
+	size_total = nmemb * size;
 
+	ptr = malloc(size_total);
 
-
-
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+	for (i = 0; i <= size_total; i++)
+	{
+		*((char *)ptr + i) = 0;
+	}
+	return (ptr);
 }
