c728dfff99cc25dc533f2497bff0f51373ab551c
Brahian-A <10114@holbertonstudents.com>
2024-11-15 13:45:46 +0000

    commit_message

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ *init_dog- funcion que inicializa variables
+ *@dog: variable
+ *@name: nombre
+ *@age: edad
+ *owner: dueÃo
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	-> dog = dog;
+	-> name = name;
+	-> owner = owner;
+}
