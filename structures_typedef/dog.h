b399c424991f65afa9a32c64fff00a88ac9b2ffd
Brahian-A <10114@holbertonstudents.com>
2024-11-15 13:12:52 +0000

    commit_message

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- la estructura
+ *@name: nombre
+ *@age: edad
+ *@owner: due√o
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
