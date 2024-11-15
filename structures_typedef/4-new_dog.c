5c6cafb86767efd7a4ee768c9a719f17969d4037
Brahian-A <10114@holbertonstudents.com>
2024-11-15 16:12:49 +0000

    commit_message

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *struct dog- la estructura
+ *@name: nombre
+ *@age: edad
+ *@owner: dueo
+ */
+
+typedef struct new_dog
+{
+
+        char *name;
+        float age;
+        char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+
+
+
+}
