88616c5aa02f8fa9c80d3db7e30bd19b9ca50b60
Brahian-A <10114@holbertonstudents.com>
2024-11-15 14:10:45 +0000

    commit_message

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ *print_dog- funcion que devuelve si es null
+ *@d: pepe
+ */
+
+
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		return;
+	
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  *struct dog- la estructura
  *@name: nombre
@@ -16,4 +17,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
