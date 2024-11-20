752c8ac0bcc3c6f52b36b465676480d5344b55b4
Brahian-A <10114@holbertonstudents.com>
2024-11-20 21:38:38 +0000

    commit_message

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,26 +1,54 @@
-#ifndef DOG_H
-#define DOG_H
 #include <stdlib.h>
 #include <stdio.h>
+#include "dog.h"
 /**
- *struct dog- la estructura
+ *new_dog- la estrucutra nueva
  *@name: nombre
  *@age: edad
  *@owner: dueo
+ *Return: pepe, los valores de la nueva estructura
  */
-
-typedef struct new_dog
-{
-
-        char *name;
-        float age;
-        char *owner;
-} dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
 
+	int long1 = 0, long2 = 0, i;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
+		while (name[long1] != '\0')
+			long1++;
+		while (owner[long2] != '\0')
+			long2++;
 
+		dog = (dog_t *)malloc(sizeof(dog_t));
+				if (dog == NULL)
+				{
+					return (NULL);
+				}
+		dog->name = malloc(long1 + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+			for (i = 0; i <= long1; i++)
+			{
+				dog->name[i] = name[i];
+			dog->owner = malloc(long2 + 1);
+			}
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+			for (i = 0; i <= long2; i++)
+			dog->owner[i] = owner[i];
+			dog->age = age;
+			return (dog);
 }
