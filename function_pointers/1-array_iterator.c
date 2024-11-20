5532dfa6e3476c9671953ab4582dbdfa9792948d
Brahian-A <10114@holbertonstudents.com>
2024-11-20 18:33:15 +0000

    commit_message

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator- funcion que ejecuta action sobre todos los elementos de array
+ *@array:array de enteros
+ *@size: tamanio del array
+ *@action: puntero a la funcion
+ *Return:void
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+
+}
