0bd973113ad0fb24c9a1a16011cf82df53f004e7
Brahian-A <10114@holbertonstudents.com>
2024-11-20 18:56:46 +0000

    commit_message

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ *int_index- funcion que busca enteros
+ *@array:array con los valores a buscar
+ *@cmp:puntero a la funcion
+ *@size: tamanio del array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size <= 0)
+	{
+		return (-1);
+	}
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	return (-1);
+}
