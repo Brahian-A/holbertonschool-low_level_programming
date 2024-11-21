4069a42ac26c32469dad622ad1029170faddacc3
Brahian-A <10114@holbertonstudents.com>
2024-11-21 23:07:38 +0000

    commit_message

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
 /**
  *
  *
@@ -11,6 +11,26 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list argu;
+	unsigned int i;
+	int num;
+	
+	va_start(argu, n);
 
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(argu, int);
+	}
+	if (separator != NULL)	
+	{
+		printf("%d", num);
+       		printf("%s", separator);
+        }
+	else 
+	{
+		printf("%d", num);
+	}
 
+    va_end(argu);
+    printf("\n");
 }
