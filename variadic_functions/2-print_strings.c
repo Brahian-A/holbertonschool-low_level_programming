cee45ca789219cbb0e6c10919ddaef20a776288b
Brahian-A <10114@holbertonstudents.com>
2024-11-22 20:20:05 +0000

    commit_message

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			num = va_arg(argu, char *);
 
-			if (num == NULL)
+			if (*num == NULL)
 			{
 				printf("nil");
 			}
