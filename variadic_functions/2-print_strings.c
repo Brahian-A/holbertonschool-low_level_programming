a5eced74109d6255d09cc4567669084bbbeb8090
Brahian-A <10114@holbertonstudents.com>
2024-11-22 20:10:37 +0000

    commit_message

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+
+/**
+ *print_strings- funcion que printea strings
+ *@separator: separador entre strings
+ *@n: cantidad de strings
+ *Return: string separador string
+ */
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list argu;
+	unsigned int i;
+	char *num;
+	unsigned int f = n;
+
+	va_start(argu, n);
+
+		for (i = 0; i < n; i++)
+		{
+			num = va_arg(argu, char *);
+
+			if (num == NULL)
+			{
+				printf("nill");
+			}
+
+			if (separator != NULL)
+			{
+				printf("%s", num);
+				if (i == f - 1)
+				{
+					break;
+				}
+				printf("%s", separator);
+			}
+
+			else
+			{
+				printf("%s", num);
+			}
+		}
+	va_end(argu);
+	printf("\n");
+}
