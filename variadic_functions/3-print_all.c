a5059ab7d2401ddc499cbd6d47b8ad4c5b72554e
Brahian-A <10114@holbertonstudents.com>
2024-11-23 16:06:29 +0000

    commit_message

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -39,6 +39,9 @@ void print_all(const char * const format, ...)
 					printf("%s", string);
 					break;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
 		i++;
 		va_end(argu);
 	}
