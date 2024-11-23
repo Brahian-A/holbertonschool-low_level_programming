31f2e41befa117f2a149a57f9c1b8ebda8d745f8
Brahian-A <10114@holbertonstudents.com>
2024-11-23 16:04:02 +0000

    commit_message

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,9 +2,7 @@
 
 /**
  *print_all- funcion que printea
- *
- *
- *
+ *@format: el formato a imprimir
  *
  */
 
@@ -15,38 +13,31 @@ void print_all(const char * const format, ...)
 	va_list argu;
 	int i = 0;
 	char *string;
-	char c;
-	int num;
-	float f;
 
 	va_start(argu, format);
 
 	while (format[i])
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
-			c = va_arg(argu, int);
-			printf("%c", c);
-		}
-
-		else if (format[i] == 'i')
-		{
-			num = va_arg(argu, int);
-			printf("%d", num);
-		}
-		else if (format[i] == 'f')
-		{
-			f = va_arg(argu, double);
-			printf("%f", f);
-		}
-
-		else if (format[i] == 's')
-		{
-			string = va_arg(argu, char *);
-			if (string != NULL)
-				printf("%s", string);
-		
-			else printf("(nil)");
+			case 'i':
+				printf("%i", va_arg(argu, int));
+			break;
+			case 'f':
+				printf("%f", va_arg(argu, double));
+			break;
+			case 'c':
+				printf("%c", (char) va_arg(argu, int));
+			break;
+			case 's':
+				string = va_arg(argu, char *);
+				if (string == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+					printf("%s", string);
+					break;
 		}
 		i++;
 		va_end(argu);
