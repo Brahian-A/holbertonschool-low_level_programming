6598e8242376610c668dcc4282d715d143c06c7e
root <10114@holbertonstudents.com>
2024-12-04 21:36:33 +0000

    el ejercicio 0 de nodos

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,18 +1,26 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- *
- *
- *
- *
- *
+ *print_list- funcion que imprime los elementos de una lista
+ *@h: puntero h que apunta al primer nodo de la lista
+ *Return: El numeros de nodos impresos
  */
 
 size_t print_list(const list_t *h)
 {
-	int c_nodos;
+	size_t conteo = 0;
 
+	if (h == NULL)
+		return (0);
 
-
-
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		conteo++;
+		h = h->next;
+	}
+	return (conteo);
 }
