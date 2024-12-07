1945fd8078211247bba88a5d390c633bbb53ae30
root <10114@holbertonstudents.com>
2024-12-07 18:54:57 +0000

    commit message

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  *print_dlistint- funcion que printea una lista enlazada
- *h@: puntero a la direccion de memoria al primer nodo
+ *@h: puntero a la direccion de memoria al primer nodo
  *Return: conteo de la lista
  */
 
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t contador = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
