b6785009cdc6652dcc8784126f189248472eee1d
root <10114@holbertonstudents.com>
2024-12-08 01:39:28 +0000

    commit message

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index- funcion que retorna el nodo en la pocicion index
+ *@head: puntero al nodo en la primer poccion
+ *@index: pocicion donde hay que retornar
+ *Return: el nodo en la pocicion de index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
